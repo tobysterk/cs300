f66db2755432f923a3a9cf1a9328664022eef1d9
tobysterk <tobysterk@gmail.com>
2020-03-25 22:31:22 +0000

    Finished lab7

--- lab7/LED-mqtt.py
+++ lab7/LED-mqtt.py
@@ -1,46 +1,63 @@
 # CS300 MQTT Lab
 import RPi.GPIO as GPIO
+import time as time
 import paho.mqtt.client as mqtt
 
 # Constants
 BROKER = 'mqtt.eclipse.org'
 PORT = 1883
 QOS = 0
-LED = 16
-MESSAGE = 'Button pressed'
+LEDY = 16
+LEDR = 15
+TOPIC_YELLOW = 'dma2/LEDY'
+TOPIC_RED = 'dma2/LEDR'
+
 
 # Setup GPIO mode
 GPIO.setmode(GPIO.BCM)
 
 # Configure GPIO for LED output
-GPIO.setup(LED, GPIO.OUT)
+GPIO.setup(LEDY, GPIO.OUT)
+GPIO.setup(LEDR, GPIO.OUT)
 
 # Callback when a connection has been established with the MQTT broker
 def on_connect(client, userdata, rc, *extra_params):
- print('Connected with result code='+str(rc))
+    print('Connected with result code='+str(rc))
 
 # Callback when client receives a message from the broker
 # Use button message to turn LED on/off
 def on_message(client, data, msg):
- if msg.topic == "ths6/button":
- if GPIO.input(LED) == 1:
- GPIO.output(LED, 0)
- else:
- GPIO.output(LED, 1)
+    print(msg.topic)
+    if msg.topic == TOPIC_YELLOW:
+        if GPIO.input(LEDY) == 1:
+            GPIO.output(LEDY, 0)
+        else:
+            GPIO.output(LEDY, 1)
+		
+    else:
+        if GPIO.input(LEDR) == 0:
+            GPIO.ouput(LEDR, 1)
+        else:
+            GPIO.output(LEDR, 0)
 
-# Setup MQTT client and callbacks
-client = mqtt.Client()
-client.on_connect = on_connect
-client.on_message = on_message
+#Main program
+try:
+  # Setup MQTT client and callbacks
+  client = mqtt.Client()
+  client.on_connect = on_connect
+  client.on_message = on_message
 
-# Connect to MQTT broker and subscribe to the button topic
-client.connect(BROKER, PORT, 60)
-client.subscribe("ths6/button", qos=QOS)
-client.loop_start()
-client.loop_start()
-while True:
- time.sleep(10)
+  # Connect to MQTT broker and subscribe to the button topic
+  client.connect(BROKER, PORT, 60)
+  client.subscribe(TOPIC_YELLOW, qos=QOS)
+  client.subscribe(TOPIC_RED, qos=QOS)
+  client.loop_start()
 
-print("Done")
-client.disconnect()
-GPIO.cleanup() # clean up GPIO
+  client.loop_start()
+  while True:
+      time.sleep(10)
+except KeyboardInterrupt:
+  print("Done.. program closed")
+finally:
+  client.disconnect()
+  GPIO.cleanup() # clean up GPIO
--- lab7/mqtt-button.py
+++ lab7/mqtt-button.py
@@ -1,19 +1,24 @@
 # CS300 MQTT Lab
 import RPi.GPIO as GPIO
 import paho.mqtt.client as mqtt
+import time
 
 # Constants
 BROKER = 'mqtt.eclipse.org' # Set the MQTT broker (change if needed)
 PORT = 1883
 QOS = 0
-IN = 12
-MESSAGE = 'Button pressed'
+SW_YELLOW = 12
+TOPIC_YELLOW = 'dma2/LEDY'
+SW_RED = 16
+TOPIC_RED = 'dma2/LEDR'
+MESSAGE = 'Button pushed'
 
 # Setup GPIO mode
 GPIO.setmode(GPIO.BCM)
 
 # Use GPIO 12 as button inputs
-GPIO.setup(IN, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(SW_YELLOW, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(SW_RED, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
 # Setup MQTT client and callbacks
 client = mqtt.Client()
@@ -21,18 +26,43 @@ client.connect(BROKER, PORT, 60)
 
 # Callback function when button is pressed
 def button_callback(channel):
- global client
- (result, num) = client.publish('ths6/button', MESSAGE, qos=QOS)
- if result != 0:
- print('PUBLISH returned error:', result)
+    global client
+    
+    # Determine topic based on channel
+    topic = ""
+    if channel == SW_YELLOW :
+        topic = TOPIC_YELLOW
+    elif channel == SW_RED:
+        topic = TOPIC_RED
+
+    # Publish message
+    (result, num) = client.publish(topic, MESSAGE, qos=QOS)
+
+    # Print feedback
+    if result != 0:
+        print('PUBLISH returned error:', result)
+    else :
+        if topic == TOPIC_YELLOW:
+            print("Yellow button pressed")
+        elif topic == TOPIC_RED:
+            print("Red button pressed")
 
 # Detect a falling edge on input pin
-GPIO.add_event_detect(IN, GPIO.FALLING, callback=button_callback, bouncetime=500)
+GPIO.add_event_detect(SW_YELLOW, GPIO.FALLING, callback=button_callback, bouncetime=500)
+GPIO.add_event_detect(SW_RED, GPIO.FALLING, callback=button_callback, bouncetime=500)
 
-client.loop_start()
-while True:
- time.sleep(10)
- 
-print("Done")
-client.disconnect()
-GPIO.cleanup() # clean up GPIO
+# Main loop
+try:
+    client.loop_start()
+    while True:
+        time.sleep(10)
+
+    print("Done")
+    client.disconnect()
+    GPIO.cleanup() # clean up GPIO
+
+except KeyboardInterrupt:
+    print("") # Prints a newline so the terminal looks good
+finally:
+    client.disconnect()
+    GPIO.cleanup()
